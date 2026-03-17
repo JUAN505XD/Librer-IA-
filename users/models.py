080db3395b6fbdda0a4f6fe0323e23611ca87c88
juanch0de <juan.henao6@utp.edu.co>
2026-03-17 14:46:55 -0500

    No se puede registrar con una fecha anterior a 100 anios ni mayor al dia de hoy

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.exceptions import ValidationError
 
 # =========================
 # MANAGER DE USUARIO
@@ -80,13 +81,24 @@ class Usuario(AbstractBaseUser):
 # =========================
 
 class Persona(models.Model):
+    def validar_fecha_nacimiento(value):
+        hoy = date.today()
+        anio_limite = hoy.year - 100
+
+        if value > hoy:
+            raise ValidationError("La fecha no es actual")
+
+        if value.year < anio_limite:
+            raise ValidationError("La fecha no puede ser tan antigua")
 
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
 
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
 
-    fecha_nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField(
+            validators=[validar_fecha_nacimiento])
+
     lugar_nacimiento = models.CharField(max_length=100)
 
     sexo = models.CharField(max_length=1)
