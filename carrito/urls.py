ab6f5471d722e00a6cfb2a5b7dd128ea7f062843
juanch0de <juan.henao6@utp.edu.co>
2026-04-23 20:02:57 -0500

    Historial de Compras y Comprar carro

--- carrito/urls.py
+++ carrito/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('agregar/<int:libro_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
     path('vaciar/', views.vaciar_carrito, name='vaciar_carrito'),
     path('pagar/', views.pagar_carrito, name='pagar_carrito'),
-    path('historial/', views.historial_compras, name='historial_compras'),
+    path('historial/', views.historial_compras, name='historial'),
 ]
