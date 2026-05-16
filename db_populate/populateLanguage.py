50477a7520c4936dd7038321dd3c99bc707ebf0f
juanch0de <juan.henao6@utp.edu.co>
2026-05-15 19:24:27 -0500

    generos and idiomas now stored as JSON

--- db_populate/populateLanguage.py
+++ db_populate/populateLanguage.py
@@ -20,5 +20,6 @@ for materia_principal in materias.find_all('h3'):
     if materia_principal.string == "Libros por idioma":
     # The first sibling is the newline, the second sibling is the actual next tag, the <ul> tag with its strings
         for idioma in materia_principal.next_sibling.next_sibling.stripped_strings:
-            idiomas.append(idioma)
+            if idioma != "Ver más...":
+                idiomas.append(idioma)
 
