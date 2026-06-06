d5a4be48b228fe0190a110a22e4ce62cc851867f
Juan505xd <Juan.garcia20@utp.edu.co>
2026-06-06 18:32:24 -0500

    gestion de libros

--- libros/urls.py
+++ libros/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("crear-autor/", views.crear_autor, name="crear_autor"),
     path("crear-genero/", views.crear_genero, name="crear_genero"),
     path("crear-editorial/", views.crear_editorial, name="crear_editorial"),
+    path("editar-libro/<int:libro_id>/",views.editar_libro,name="editar_libro"),
+    path("eliminar-libro/<int:libro_id>/",views.eliminar_libro,name="eliminar_libro"),
 ]
